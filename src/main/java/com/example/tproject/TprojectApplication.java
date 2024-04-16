b9cfed7b5110fbf65e28c59d2010c9c9c8f3af1d
olimarel <olimarel@gmail.com>
2024-04-16 12:44:29 +0000

    Laga viðmót fyrir fullscreen

--- src/main/java/com/example/tproject/TprojectApplication.java
+++ src/main/java/com/example/tproject/TprojectApplication.java
@@ -4,17 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class TprojectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TprojectApplication.class.getResource("DateSelector-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1500, 1000);
         stage.setTitle("T-project");
         ViewSwitcher.setScene(scene);
         ViewSwitcher.pushToHistory(View.DATESELECTOR);   //bætt við
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
