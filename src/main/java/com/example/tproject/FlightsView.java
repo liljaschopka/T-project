33a45ca0b03e0ead0950db85f2f3e8b992ea99cf
lijaschopka <123467927+liljaschopka@users.noreply.github.com>
2024-04-17 13:09:25 +0000

    Update FlightsView.java

--- src/main/java/com/example/tproject/FlightsView.java
+++ src/main/java/com/example/tproject/FlightsView.java
@@ -75,8 +75,8 @@ public class FlightsView {
             @Override
             protected void updateItem(Flight flight, boolean empty) {
                 super.updateItem(flight, empty);
-                setText(empty || flight == null ? null : (flight.getOrigin() + "-" +
-                        flight.getDestination() + ", " + flight.getDepartureTime() + ", " + flight.getPrice() + " ISK per person"));
+                setText(empty || flight == null ? null : (flight.getOrigin() + "-" + flight.getDestination() + ", " + flight.getDepartureTime() + ", " + flight.getPrice() + " ISK" + ", available seats: " + flight.getAvailableSeats()))
+                ;
             }
         });
 
@@ -90,8 +90,8 @@ public class FlightsView {
             @Override
             protected void updateItem(Flight flight, boolean empty) {
                 super.updateItem(flight, empty);
-                setText(empty || flight == null ? null : (flight.getOrigin() + "-" +
-                        flight.getDestination() + ", " + flight.getDepartureTime() + ", " + flight.getPrice() + " ISK per person"));
+                setText(empty || flight == null ? null : (flight.getOrigin() + "-" + flight.getDestination() + ", " + flight.getDepartureTime() + ", " + flight.getPrice() + " ISK" + ", available seats: " + flight.getAvailableSeats()))
+
             }
         });
 
